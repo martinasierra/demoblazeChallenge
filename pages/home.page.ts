67c4cdf81e22d2a359e10d29a1d1200286d1e22a
martinasierra <martinasierra94@gmail.com>
2024-09-22 18:53:14 -0300

    Update tests

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -5,12 +5,14 @@ readonly page:Page;     //Variables
 readonly cartButton:Locator;
 readonly xperiaButton:Locator;
 readonly categoriesSection:Locator;
+readonly nextPageButton:Locator;
 
   constructor(page: Page) {
 this.page = page;
 this.xperiaButton = page.getByRole('link', { name: 'Sony xperia z5' });
 this.cartButton = page.getByRole('link', { name: 'Cart', exact: true });
 this.categoriesSection = page.getByRole('link', { name: 'CATEGORIES' });
+this.nextPageButton = page.getByRole('button', { name: 'Next' }).last();
 
   };
 
@@ -24,6 +26,9 @@ this.categoriesSection = page.getByRole('link', { name: 'CATEGORIES' });
     await this.cartButton.click();
  };
 
+ async clickNext() {
+   await this.nextPageButton.click();
+ }
   
 };
 
